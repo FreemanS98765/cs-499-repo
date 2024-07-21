ff8a6c77529d64226ff2b86ce9b56362f2103f4f
Freeman Sands <sandsfreeman@gmail.com>
2024-07-21 07:50:31 -0400

    Simple form setup

---
+++ app_public/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Email: ' + this.loginForm.value.email);
+      console.log('Password: ' + this.loginForm.value.password);
+    }
+  }
+}
