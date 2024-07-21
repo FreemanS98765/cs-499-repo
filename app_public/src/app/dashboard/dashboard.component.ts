d30fc1fb6651b4345ac61a77dd72e1839105c9f5
Freeman Sands <sandsfreeman@gmail.com>
2024-07-21 13:57:34 -0400

    Simple add product dialog and form

--- app_public/src/app/dashboard/dashboard.component.ts
+++ app_public/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
 import { InventoryService } from '../services/inventory.service';
 import { InventoryItem } from '../models/inventory-item.model';
+import { CreateProductComponent } from '../create-product/create-product.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   host: { class: 'dashboard-view' },
@@ -14,7 +17,10 @@ import { InventoryItem } from '../models/inventory-item.model';
 export class DashboardComponent implements OnInit {
   inventory: InventoryItem[] = [];
 
-  constructor(private inventoryService: InventoryService) {}
+  constructor(
+    private inventoryService: InventoryService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.loadInventory();
@@ -24,6 +30,18 @@ export class DashboardComponent implements OnInit {
     this.inventoryService.getInventory().subscribe((data) => {
       this.inventory = data;
       console.log(this.inventory);
+    });
+  }
+
+  openCreateProductDialog(): void {
+    const dialogRef = this.dialog.open(CreateProductComponent, {
+      width: '400px',
+      data: { name: '', sku: '', quantity: '' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      console.log(result);
     });
   }
 }
