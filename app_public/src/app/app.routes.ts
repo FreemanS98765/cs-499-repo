3c7bc205616709882433c070342c24c353074ed0
Freeman Sands <sandsfreeman@gmail.com>
2024-07-25 08:07:32 -0400

    Reconfigured routing and set up for notification component

--- app_public/src/app/app.routes.ts
+++ app_public/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
-export const routes: Routes = [
+export const routes: Routes  = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: 'notifications', component: NotificationsComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
