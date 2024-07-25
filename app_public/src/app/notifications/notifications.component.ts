077d60f6ac6d79322bd2777f724ed8aa567ca2e2
Freeman Sands <sandsfreeman@gmail.com>
2024-07-25 08:21:39 -0400

    Basic setup of notification template

--- app_public/src/app/notifications/notifications.component.ts
+++ app_public/src/app/notifications/notifications.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
+/**
+ * @title Notifications Component
+ */
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.css'
+  styleUrl: './notifications.component.css',
+  host: { class: 'notifications-view' },
 })
 export class NotificationsComponent {
 
