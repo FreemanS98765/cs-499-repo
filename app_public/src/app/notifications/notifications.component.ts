6d8f2c8036228e98c05334dd3685313d7bb20657
Freeman Sands <sandsfreeman@gmail.com>
2024-07-27 10:55:42 -0400

    Notifications view set up

--- app_public/src/app/notifications/notifications.component.ts
+++ app_public/src/app/notifications/notifications.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 /**
  * @title Notifications Component
@@ -8,11 +14,30 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatSlideToggleModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css',
   host: { class: 'notifications-view' },
 })
 export class NotificationsComponent {
+  notificationsState: boolean = true;
 
+  toggleNotifications(event: any): void {
+    this.notificationsState = event.checked;
+  }
+
+  notifications = [
+    { title: 'Deleted item: New Product', date: '2024-06-23 21:08:43' },
+    { title: 'Deleted item: sdfas', date: '2024-06-23 21:08:45' },
+    { title: 'New item added: dsaf', date: '2024-06-23 21:08:57' },
+  ];
 }
